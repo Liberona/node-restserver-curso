ee570ffdad26ebe9f988188f5e5477e90bab6934
liberona <pablo.liberona.s@gmail.com>
2018-07-31 19:37:38 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:123abc@ds115738.mlab.com:15738/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
